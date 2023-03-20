31a0e48cdc4224fb0cc71a4d53b431cdc5896f32
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-03-20 22:23:44 +0200

    creat style heder and card clients

--- src/components/Home/styled/Home.styled.jsx
+++ src/components/Home/styled/Home.styled.jsx
@@ -1,21 +1,20 @@
 import styled from '@emotion/styled';
 
 export const CONTENT_DIV = styled.div`
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  display: grid;
+  place-items: center;
+  gap: 0 100px;
+  grid-template-columns: 250px 250px 250px;
+  justify-content: center;
+  @media screen and (max-width: 780px) {
+    grid-template-columns: 200px 200px;
+    gap: 0;
+  }
 `;
 
 export const CLIENTS_DIV = styled.div`
-  margin: 10px;
-
   @media screen and (min-width: 760px) {
     padding: 10px;
-    margin: 20px;
-  }
-  @media screen and (min-width: 1200px) {
-    margin: 30px;
   }
 
   img {
