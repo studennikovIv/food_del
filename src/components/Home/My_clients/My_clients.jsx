158f006a376862b464fe9cb33f2cbb0f5f677436
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-05-04 20:16:46 +0300

    hg

--- src/components/Home/My_clients/My_clients.jsx
+++ src/components/Home/My_clients/My_clients.jsx
@@ -1,42 +1,56 @@
-import sushiFamily from './images/sushiFamily.jpg';
-import KFC from './images/KFC.jpg';
-import myCherie from './images/MyCherie.jpg';
-import Shaurmichka from './images/Shaurmichka.jpg';
-import pizzaDay from './images/pizzaDay.jpg';
-import MyPlace from './images/MyPlace.jpg';
+// import sushiFamily from './images/sushiFamily.jpg';
+// import KFC from './images/KFC.jpg';
+// import myCherie from './images/MyCherie.jpg';
+// import Shaurmichka from './images/Shaurmichka.jpg';
+// import pizzaDay from './images/pizzaDay.jpg';
+// import MyPlace from './images/MyPlace.jpg';
 // import sushiZoom from './images/sushiZoom.jpg';
 // import osamaSushi from './images/osamaSushi.jpg';
 
+
+import pizzaDay2 from './images/pizza day.jpg'
+import KFC2 from './images/FFC.jpg'
+import myPlace2 from './images/my Place.jpg'
+import maCherie2 from './images/Ma Cherie.jpg'
+import sushiFamily2 from './images/sushi family.jpg'
+import shaurmichka2 from './images/SHAVARMA.jpg'
+import laziz from './images/laziz.jpg'
 const myClients = [
   {
     id: 1,
     name: 'SushiFamily',
-    logoImg: sushiFamily,
+    // logoImg: sushiFamily,
+    logoImg: sushiFamily2
   },
   {
     id: 2,
     name: 'KFC',
-    logoImg: KFC,
+    // logoImg: KFC,
+    logoImg: KFC2 
   },
   {
     id: 3,
     name: 'Shaurmichka',
-    logoImg: Shaurmichka,
+    // logoImg: Shaurmichka,
+    logoImg: shaurmichka2
   },
   {
     id: 4,
     name: 'MaCherie',
-    logoImg: myCherie,
+    // logoImg: myCherie,
+    logoImg:maCherie2,
   },
   {
     id: 6,
     name: 'PizzaDay',
-    logoImg: pizzaDay,
+    // logoImg: pizzaDay,
+    logoImg:pizzaDay2
   },
   {
     id: 7,
     name: 'MyPlace',
-    logoImg: MyPlace,
+    // logoImg: MyPlace,
+    logoImg:myPlace2
   },
   // {
   //   id: 8,
