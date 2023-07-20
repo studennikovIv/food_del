36c1f171cc8846dd661291b610d93b495af64a59
grant956 <68757554+studennikovIv@users.noreply.github.com>
2023-07-20 21:25:53 +0300

    fix bag

--- src/components/Basket/card/BasketFormClient.jsx
+++ src/components/Basket/card/BasketFormClient.jsx
@@ -25,70 +25,69 @@ import {
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../../store/index';
 
-export const BasketFormClient = observer(
-  ({ food, total, basketFormClient }) => {
-    const { basketStore } = useStore();
-    const [changeTel] = useState('');
-    const [changeStreat] = useState('');
-    const [changeHouse, setChangeHouse] = useState('');
-    const [changeComents] = useState('');
-    const [phoneActive, setPhoneActive] = useState(null);
-    const [streatActive, setStreatActive] = useState(null);
-    const [houseActive, setHouseActive] = useState(null);
-    const [comentsActive, setComentsActive] = useState(null);
+export const BasketFormClient = observer(({ food, basketFormClient }) => {
+  const { basketStore } = useStore();
+  const [changeTel] = useState('');
+  const [changeStreat] = useState('');
+  const [changeHouse, setChangeHouse] = useState('');
+  const [changeComents, setChangeComents] = useState('');
+  const [phoneActive, setPhoneActive] = useState(null);
+  const [streatActive, setStreatActive] = useState(null);
+  const [houseActive, setHouseActive] = useState(null);
+  const [comentsActive, setComentsActive] = useState(null);
 
-    const {
-      control,
-      handleSubmit,
-      formState: { errors },
-    } = useForm();
-    console.log(errors);
-    const location = useLocation();
-    const kafe = location.pathname;
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  console.log(errors);
+  const location = useLocation();
+  const kafe = location.pathname;
 
-    const onSubmit = async e => {
-      // e.preventDefault();
-      const resetBasket = basketStore.resetBasket;
-      basketStore.openBusket = false;
-      console.log(basketStore.getBasketItems);
-      console.log(e);
-      const { telephone, streat, house, coments } = e;
-      await botService.sendMessage(
-        telephone,
-        streat,
-        house,
-        coments,
-        food,
-        total,
-        kafe
-      );
+  const onSubmit = async e => {
+    const total = basketStore.sum;
+    basketStore.openBusket = false;
 
-      e.telephone = '';
-      resetBasket([]);
-    };
+    const { telephone, streat, house } = e;
+    const coments = changeComents;
+    console.log(e);
+    await botService.sendMessage(
+      telephone,
+      streat,
+      house,
+      coments,
+      food,
+      total,
+      kafe
+    );
 
-    return (
-      <>
-        <ClientForm onSubmit={handleSubmit(onSubmit)}>
-          <ClientUl>
-            <ClientLi>
-              <ClientLabel htmlFor="telephone">
-                <ClientSmall
-                  className={
-                    phoneActive === true || changeTel !== '' ? 'active' : null
-                  }
-                >
-                  Телефон 380000000000
-                </ClientSmall>
-                <ClientSup
-                  className={
-                    phoneActive === true || changeTel !== '' ? 'active' : null
-                  }
-                >
-                  *
-                </ClientSup>
-              </ClientLabel>
-              {/* <ClientInput
+    e.telephone = '';
+    basketStore.resetBasket();
+  };
+
+  return (
+    <>
+      <ClientForm onSubmit={handleSubmit(onSubmit)}>
+        <ClientUl>
+          <ClientLi>
+            <ClientLabel htmlFor="telephone">
+              <ClientSmall
+                className={
+                  phoneActive === true || changeTel !== '' ? 'active' : null
+                }
+              >
+                Телефон 380000000000
+              </ClientSmall>
+              <ClientSup
+                className={
+                  phoneActive === true || changeTel !== '' ? 'active' : null
+                }
+              >
+                *
+              </ClientSup>
+            </ClientLabel>
+            {/* <ClientInput
               onBlur={() => setPhoneActive(null)}
               onChange={e => {
                 setPhoneActive(true);
@@ -100,57 +99,57 @@ export const BasketFormClient = observer(
               pattern="[0-9]*"
               required
             /> */}
-              <Controller
-                control={control}
-                name="telephone"
-                render={({ field }) => (
-                  <ClientInput
-                    {...field}
-                    onBlur={() => setPhoneActive(null)}
-                    onChange={e => {
-                      setPhoneActive(true);
-                      field.onChange(e);
-                    }}
-                    onClick={() => setPhoneActive(true)}
-                    type="number"
-                    pattern="[0-9]*"
-                    required
-                  />
-                )}
-              />
-            </ClientLi>
+            <Controller
+              control={control}
+              name="telephone"
+              render={({ field }) => (
+                <ClientInput
+                  {...field}
+                  onBlur={() => setPhoneActive(null)}
+                  onChange={e => {
+                    setPhoneActive(true);
+                    field.onChange(e);
+                  }}
+                  onClick={() => setPhoneActive(true)}
+                  type="number"
+                  pattern="[0-9]*"
+                  required
+                />
+              )}
+            />
+          </ClientLi>
+          <ClientLi>
+            <ClientLabel htmlFor="telephone">
+              <ClientSmall className={'active'}>Місто</ClientSmall>
+              <ClientSup className={'active'}>*</ClientSup>
+            </ClientLabel>
+            <ClientSelect>
+              <option>Павлоград</option>
+            </ClientSelect>
+          </ClientLi>
+          <ClientConteinerStreetDiv>
             <ClientLi>
-              <ClientLabel htmlFor="telephone">
-                <ClientSmall className={'active'}>Місто</ClientSmall>
-                <ClientSup className={'active'}>*</ClientSup>
+              <ClientLabel htmlFor="streat">
+                <ClientSmall
+                  className={
+                    streatActive === true || changeStreat !== ''
+                      ? 'active'
+                      : null
+                  }
+                >
+                  Вулиця
+                </ClientSmall>
+                <ClientSup
+                  className={
+                    streatActive === true || changeStreat !== ''
+                      ? 'active'
+                      : null
+                  }
+                >
+                  *
+                </ClientSup>
               </ClientLabel>
-              <ClientSelect>
-                <option>Павлоград</option>
-              </ClientSelect>
-            </ClientLi>
-            <ClientConteinerStreetDiv>
-              <ClientLi>
-                <ClientLabel htmlFor="streat">
-                  <ClientSmall
-                    className={
-                      streatActive === true || changeStreat !== ''
-                        ? 'active'
-                        : null
-                    }
-                  >
-                    Вулиця
-                  </ClientSmall>
-                  <ClientSup
-                    className={
-                      streatActive === true || changeStreat !== ''
-                        ? 'active'
-                        : null
-                    }
-                  >
-                    *
-                  </ClientSup>
-                </ClientLabel>
-                {/* <ClientInput
+              {/* <ClientInput
                 onBlur={() => setStreatActive(null)}
                 onChange={e => {
                   setStreatActive(true);
@@ -164,48 +163,44 @@ export const BasketFormClient = observer(
                 pattern=".*"
                 required
               /> */}
-                <Controller
-                  control={control}
-                  name="streat"
-                  render={({ field }) => (
-                    <ClientInput
-                      {...field}
-                      onBlur={() => setStreatActive(null)}
-                      onChange={e => {
-                        setStreatActive(true);
-                        field.onChange(e);
-                      }}
-                      onClick={() => setStreatActive(true)}
-                      type="text"
-                      pattern=".*"
-                      required
-                    />
-                  )}
-                />
-              </ClientLi>
+              <Controller
+                control={control}
+                name="streat"
+                render={({ field }) => (
+                  <ClientInput
+                    {...field}
+                    onBlur={() => setStreatActive(null)}
+                    onChange={e => {
+                      setStreatActive(true);
+                      field.onChange(e);
+                    }}
+                    onClick={() => setStreatActive(true)}
+                    type="text"
+                    pattern=".*"
+                    required
+                  />
+                )}
+              />
+            </ClientLi>
 
-              <ClientLi>
-                <ClientLabel htmlFor="house">
-                  <ClientSmall
-                    className={
-                      houseActive === true || changeHouse !== ''
-                        ? 'active'
-                        : null
-                    }
-                  >
-                    Будинок
-                  </ClientSmall>
-                  <ClientSup
-                    className={
-                      houseActive === true || changeHouse !== ''
-                        ? 'active'
-                        : null
-                    }
-                  >
-                    *
-                  </ClientSup>
-                </ClientLabel>
-                {/* <ClientInput
+            <ClientLi>
+              <ClientLabel htmlFor="house">
+                <ClientSmall
+                  className={
+                    houseActive === true || changeHouse !== '' ? 'active' : null
+                  }
+                >
+                  Будинок
+                </ClientSmall>
+                <ClientSup
+                  className={
+                    houseActive === true || changeHouse !== '' ? 'active' : null
+                  }
+                >
+                  *
+                </ClientSup>
+              </ClientLabel>
+              {/* <ClientInput
                 onBlur={() => setHouseActive(null)}
                 onChange={e => {
                   setHouseActive(true);
@@ -219,42 +214,42 @@ export const BasketFormClient = observer(
                 pattern=".*"
                 required
               /> */}
-                <Controller
-                  control={control}
-                  name="house"
-                  render={({ field }) => (
-                    <ClientInput
-                      {...field}
-                      onBlur={() => setHouseActive(null)}
-                      onChange={e => {
-                        setHouseActive(true);
-                        setChangeHouse(e.target.value);
-                        field.onChange(e);
-                      }}
-                      onClick={() => setHouseActive(true)}
-                      type="number"
-                      pattern=".*"
-                      required
-                    />
-                  )}
-                />
-              </ClientLi>
-            </ClientConteinerStreetDiv>
+              <Controller
+                control={control}
+                name="house"
+                render={({ field }) => (
+                  <ClientInput
+                    {...field}
+                    onBlur={() => setHouseActive(null)}
+                    onChange={e => {
+                      setHouseActive(true);
+                      setChangeHouse(e.target.value);
+                      field.onChange(e);
+                    }}
+                    onClick={() => setHouseActive(true)}
+                    type="number"
+                    pattern="*"
+                    required
+                  />
+                )}
+              />
+            </ClientLi>
+          </ClientConteinerStreetDiv>
 
-            <ClientLi>
-              <ClientLabel htmlFor="coments">
-                <ClientSmall
-                  className={
-                    comentsActive === true || changeComents !== ''
-                      ? 'active'
-                      : null
-                  }
-                >
-                  Коментар
-                </ClientSmall>
-              </ClientLabel>
+          <ClientLi>
+            <ClientLabel htmlFor="coments">
+              <ClientSmall
+                className={
+                  comentsActive === true || changeComents !== ''
+                    ? 'active'
+                    : null
+                }
+              >
+                Коментар
+              </ClientSmall>
+            </ClientLabel>
 
-              {/* <ClientInput
+            {/* <ClientInput
               onBlur={() => setComentsActive(null)}
               onChange={e => {
                 setComentsActive(true);
@@ -265,44 +260,43 @@ export const BasketFormClient = observer(
               name="coments"
               pattern=".*"
             /> */}
-              <Controller
-                control={control}
-                name="coments"
-                render={({ field }) => (
-                  <ClientInput
-                    {...field}
-                    onBlur={() => setComentsActive(null)}
-                    onChange={e => {
-                      setComentsActive(true);
-
-                      field.onChange(console.log(field));
-                    }}
-                    onClick={() => setComentsActive(true)}
-                    type="text"
-                    pattern=".*"
-                  />
-                )}
-              />
-            </ClientLi>
-          </ClientUl>
-          <ConteinerClientSumDiv>
-            <SumClientP>Сумма:{basketStore.sum}</SumClientP>
-            <DeliveryClientP>Доставка:50грн</DeliveryClientP>
-            <SumClientP>До сплати:{basketStore.sum + 50}</SumClientP>
-          </ConteinerClientSumDiv>
-          <ConteinerBtnClientDiv>
-            <LeftClientButton
-              onClick={e => {
-                e.preventDefault();
-                basketFormClient(false);
-              }}
-            >
-              Назад
-            </LeftClientButton>
-            <RightClientButton>Відправити замовлення</RightClientButton>
-          </ConteinerBtnClientDiv>
-        </ClientForm>
-      </>
-    );
-  }
-);
+            <Controller
+              control={control}
+              name="coments"
+              render={({ field }) => (
+                <ClientInput
+                  {...field}
+                  onBlur={() => setComentsActive(null)}
+                  onChange={e => {
+                    setComentsActive(true);
+                    setChangeComents(e.target.value);
+                    field.onChange(console.log(field));
+                  }}
+                  onClick={() => setComentsActive(true)}
+                  type="text"
+                  pattern=".*"
+                />
+              )}
+            />
+          </ClientLi>
+        </ClientUl>
+        <ConteinerClientSumDiv>
+          <SumClientP>Сумма:{basketStore.sum}</SumClientP>
+          <DeliveryClientP>Доставка:50грн</DeliveryClientP>
+          <SumClientP>До сплати:{basketStore.sum + 50}</SumClientP>
+        </ConteinerClientSumDiv>
+        <ConteinerBtnClientDiv>
+          <LeftClientButton
+            onClick={e => {
+              e.preventDefault();
+              basketFormClient(false);
+            }}
+          >
+            Назад
+          </LeftClientButton>
+          <RightClientButton>Відправити замовлення</RightClientButton>
+        </ConteinerBtnClientDiv>
+      </ClientForm>
+    </>
+  );
+});
--- src/components/Menu/card/OneCard.jsx
+++ src/components/Menu/card/OneCard.jsx
@@ -38,36 +38,8 @@ export function OneCard({ position, clickOnCard }) {
       formattedPrice: price,
     };
     return (
-      // <ONE_CARD_LI
-      //   key={name}
-      //   onClick={() => {
-      //     linkImg = img;
-      //     linkName = name;
-      //     linkPrice = price;
-      //     linkText = text;
-      //     ClickOpenModal();
-      //   }}
-      // >
-      //   <IMAGE_IMG src={img ? imagePath : notHaveFoto} alt={name} />
-      //   {/* {img ? (
-      //     <IMAGE_IMG src={imagePath} alt={name} />
-      //   ) : (
-      //     <IMAGE_IMG src={notHaveFoto} alt="food-del" />
-      //   )} */}
-      //   <NAME_DISH_H1>{name}</NAME_DISH_H1>
-      //   {text && <DESCRIPTION_P>{text}</DESCRIPTION_P>}
-      //   <SUPPORT_DIV>
-      //     {size && <SIZE_P>{size}</SIZE_P>}
-      //     {mass && <MASSA_P>{mass}</MASSA_P>}
-      //   </SUPPORT_DIV>
-      //   <BUTTON_PRICE>{price}₴</BUTTON_PRICE>
-      // </ONE_CARD_LI>
-      <Box
+      <ONE_CARD_LI
         key={name}
-        maxW="sm"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
         onClick={() => {
           linkImg = img;
           linkName = name;
@@ -76,44 +48,72 @@ export function OneCard({ position, clickOnCard }) {
           ClickOpenModal();
         }}
       >
-        <Image src={property.imageUrl} alt={property.imageAlt} />
-        <Box p="6">
-          <Box display="flex" alignItems="baseline">
-            <Badge borderRadius="full" px="2" colorScheme="teal"></Badge>
-          </Box>
+        <IMAGE_IMG src={img ? imagePath : notHaveFoto} alt={name} />
+        {/* {img ? (
+          <IMAGE_IMG src={imagePath} alt={name} />
+        ) : (
+          <IMAGE_IMG src={notHaveFoto} alt="food-del" />
+        )} */}
+        <NAME_DISH_H1>{name}</NAME_DISH_H1>
+        {text && <DESCRIPTION_P>{text}</DESCRIPTION_P>}
+        <SUPPORT_DIV>
+          {size && <SIZE_P>{size}</SIZE_P>}
+          {mass && <MASSA_P>{mass}</MASSA_P>}
+        </SUPPORT_DIV>
+        <BUTTON_PRICE>{price}₴</BUTTON_PRICE>
+      </ONE_CARD_LI>
+      // <Box
+      //   key={name}
+      //   maxW="sm"
+      //   borderWidth="1px"
+      //   borderRadius="lg"
+      //   overflow="hidden"
+      //   onClick={() => {
+      //     linkImg = img;
+      //     linkName = name;
+      //     linkPrice = price;
+      //     linkText = text;
+      //     ClickOpenModal();
+      //   }}
+      // >
+      //   <Image src={property.imageUrl} alt={property.imageAlt} />
+      //   <Box p="6">
+      //     <Box display="flex" alignItems="baseline">
+      //       <Badge borderRadius="full" px="2" colorScheme="teal"></Badge>
+      //     </Box>
 
-          <Box
-            mt="1"
-            fontWeight="semibold"
-            as="h4"
-            lineHeight="tight"
-            noOfLines={1}
-          >
-            {property.title}
-          </Box>
+      //     <Box
+      //       mt="1"
+      //       fontWeight="semibold"
+      //       as="h4"
+      //       lineHeight="tight"
+      //       noOfLines={1}
+      //     >
+      //       {property.title}
+      //     </Box>
 
-          <Box>
-            {property.formattedPrice}
-            <Box as="span" color="gray.600" fontSize="sm">
-              / ₴
-            </Box>
-          </Box>
+      //     <Box>
+      //       {property.formattedPrice}
+      //       <Box as="span" color="gray.600" fontSize="sm">
+      //         / ₴
+      //       </Box>
+      //     </Box>
 
-          <Box display="flex" mt="2" alignItems="center">
-            {Array(5)
-              .fill('')
-              .map((_, i) => (
-                <StarIcon
-                  key={i}
-                  color={i < property.rating ? 'teal.500' : 'gray.300'}
-                />
-              ))}
-            <Box as="span" ml="2" color="gray.600" fontSize="sm">
-              {property.reviewCount} reviews
-            </Box>
-          </Box>
-        </Box>
-      </Box>
+      //     <Box display="flex" mt="2" alignItems="center">
+      //       {Array(5)
+      //         .fill('')
+      //         .map((_, i) => (
+      //           <StarIcon
+      //             key={i}
+      //             color={i < property.rating ? 'teal.500' : 'gray.300'}
+      //           />
+      //         ))}
+      //       <Box as="span" ml="2" color="gray.600" fontSize="sm">
+      //         {property.reviewCount} reviews
+      //       </Box>
+      //     </Box>
+      //   </Box>
+      // </Box>
     );
   });
 }
